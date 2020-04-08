bbfa0fb9ef159bbb97e213c89324c228499bd639
santiagoserrato <santiagoserrato@gmail.com>
2020-04-08 11:07:50 -0500

    Use uuid for todo id

--- src/App.js
+++ src/App.js
@@ -1,66 +1,71 @@
 import React from 'react';
 import './App.css';
 import Todos from './Components/Todos';
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends React.Component {
-
   state = {
     todos: [
       {
-        id: 1,
-        title: 'Clean kitchen',
-        completed: false
+        id: "d99498ec-fde5-4fba-9c7e-d4936a545c6b",
+        title: "Clean kitchen",
+        completed: false,
       },
       {
-        id: 2,
-        title: 'Sweep living room',
-        completed: false
+        id: "9392eae9-3646-4804-90a9-96c5ebbdbde3",
+        title: "Sweep living room",
+        completed: false,
       },
       {
-        id: 3,
-        title: 'Wash clothes',
-        completed: false
-      }
-    ]
-  }
+        id: "126a96bb-1c6e-4a49-8f50-cbc39f62b21f",
+        title: "Wash clothes",
+        completed: false,
+      },
+    ],
+  };
 
   toggleCompleted = (id) => {
     this.setState({
-      todos: this.state.todos.map(todo => {
-        if(todo.id === id){
-          todo.completed = !todo.completed
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
         }
         return todo;
-      })
+      }),
     });
-  }
+  };
 
   addTodo = (newTitle) => {
-    if (newTitle === ''){
+    if (newTitle === "") {
       return;
     }
 
     const newTodo = {
-      id: this.state.todos.length + 1,
+      id: uuidv4(),
       title: newTitle,
-      completed: false
+      completed: false,
     };
 
     this.setState({
-      todos: [...this.state.todos, newTodo]
+      todos: [...this.state.todos, newTodo],
     });
-  }
+  };
 
   deleteTodo = (id) => {
     this.setState({
-      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
     });
-  }
+  };
 
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} toggleCompleted={this.toggleCompleted} addTodo={this.addTodo} deleteTodo={this.deleteTodo} />
+        <Todos
+          todos={this.state.todos}
+          toggleCompleted={this.toggleCompleted}
+          addTodo={this.addTodo}
+          deleteTodo={this.deleteTodo}
+        />
       </div>
     );
   }
